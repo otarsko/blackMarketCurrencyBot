7378c208d097cf54337d6c308cd19ccea3a99c8d
OTarasenko <tarasenk.alexander@gmail.com>
2016-08-07 21:28:34 +0200

    [MinFin data provider] Crawl deals + tests(in progress).

--- src/services/minfin/deal.model.js
+++ src/services/minfin/deal.model.js
@@ -1,9 +1,27 @@
 export default class Deal {
 
-    constructor() {}
+    constructor(bidId, rate, time, ammount, message) {
+        this.bidId = bidId;
+        this.rate = rate;
+        this.time = time;
+        this.ammount = ammount;
+        this.message = message;
+    }
 
-    getTime() {}
-    getRate() {}
-    getAmmount() {}
-    getAddress() {}
+    getBidId() {
+        return this.bidId;
+    }
+
+    getTime() {
+        return this.time;
+    }
+    getRate() {
+        return this.rate;
+    }
+    getAmmount() {
+        return this.ammount;
+    }
+    getMessage() {
+        return this.message;
+    }
 }
--- src/services/minfin/parsing/minfinParser.js
+++ src/services/minfin/parsing/minfinParser.js
@@ -1,18 +1,37 @@
 import Request from 'request';
 import Cheerio from 'cheerio';
+import Deal from '../deal.model';
 
 export default class MinfinParser {
 
     constructor() {
-        this.request = new Request();
         this.cheerio = new Cheerio();
     }
 
     //todo: move class names to separate config, use callback, implement
     getDeals(url, callback) {
-        request(url, function(err, resp, body) {
-            var $ = cheerio.load(body);
-            var dealsEntries = $('.js-au-deal');
+        if (!url) {
+            console.error("Got undefined url");
+            callback([]);
+            return;
+        }
+        new Request(url, function (err, resp, body) {
+            var $ = Cheerio.load(body);
+            var deals = [];
+            $('.js-au-deal').each(function(i, elem) {
+                var bidId = $(elem).data("bid");
+                var rate = $(elem).find(".au-deal-currency") && ($(elem).find(".au-deal-currency").html() || 0);
+                if (rate && bidId) {
+                    var time = $(elem).find(".au-deal-time").html();
+                    var sum = $(elem).find(".au-deal-sum").html();
+                    var message = $(elem).find(".js-au-msg-wrapper").html();
+                    deals.push(new Deal(bidId, rate, time, sum, message));
+                }
+            });
+            if (deals.length === 0) {
+                console.error("No deals found for url: " + url);
+            }
+            callback(deals);
         });
     }
 }
